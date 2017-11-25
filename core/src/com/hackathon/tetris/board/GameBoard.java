780817032069ae94477db4799b1389e159ca8790
Patrycja Bruździńska <patrycja.bruzdzinska@gmail.com>
2017-11-25 12:48:04 +0100

    Update

--- core/src/com/hackathon/tetris/board/GameBoard.java
+++ core/src/com/hackathon/tetris/board/GameBoard.java
@@ -2,14 +2,20 @@ package com.hackathon.tetris.board;
 
 
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.hackathon.tetris.blocks.Blocks;
+import com.hackathon.tetris.blocks.BlocksTypesEnum;
 import com.hackathon.tetris.blocks.SmallBlocks;
 
 import java.util.List;
 import java.util.Random;
 
+import static com.hackathon.tetris.blocks.BlocksTypesEnum.*;
+
 public class GameBoard {
-    Rectangle board;
+    int xCorner=0;
+    int yCorner=0;
+    Rectangle board=new Rectangle(xCorner,yCorner,200,200);
     List<Blocks> currentBlocks;
     Blocks activeBlock=null;
     SmallBlocks[][] gameBoard=new SmallBlocks[10][10];
@@ -33,10 +39,40 @@ public class GameBoard {
         }
         return false;
     }
-    public void generateNewBlock() {
+    public Blocks generateNewBlock() {
         if (activeBlock!=null)
             currentBlocks.add(activeBlock);
         Random random=new Random();
-        int numberOf=random.nextInt(6)+1;
+        int numberOfType=random.nextInt(7)+1;
+        BlocksTypesEnum newBlockType=null;
+        switch (numberOfType) {
+            case 1:
+                newBlockType=Z;
+                break;
+            case 2:
+                newBlockType=NAIL;
+                break;
+            case 3:
+                newBlockType=L;
+                break;
+            case 4:
+                newBlockType=Z_INVERSE;
+                break;
+            case 5:
+                newBlockType=SQUARE;
+                break;
+            case 6:
+                newBlockType=L_INVERSE;
+                break;
+            case 7:
+                newBlockType=LINE;
+        }
+        float scale=board.getWidth()/10;
+        Blocks block = new Blocks(newBlockType,scale);
+        int rotation = random.nextInt(4);
+        Vector2 position=new Vector2(xCorner+board.getWidth()/2,yCorner+board.getHeight());
+        block.setRotation(rotation);
+        block.setPosition(position);
+        return block;
     }
 }
