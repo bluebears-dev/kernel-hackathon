3101a53abe029750f54cd8fcc16f63e3504fbe3f
Patrycja Bruździńska <patrycja.bruzdzinska@gmail.com>
2017-11-25 12:16:39 +0100

    Setting texture.

--- core/src/com/hackathon/tetris/blocks/Blocks.java
+++ core/src/com/hackathon/tetris/blocks/Blocks.java
@@ -1,19 +1,42 @@
 package com.hackathon.tetris.blocks;
 
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 
 public class Blocks extends Polygon{
     private Vector2 position;
     private byte numberOfBlocks=4;
+    private Texture blockImage=null;
+    private BlocksTypesEnum type;
 
-    Blocks (BlocksTypesEnum blockType, float scale) {
+    public Blocks (BlocksTypesEnum blockType, float scale) {
+        type=blockType;
+        setTexture();
         float[] vertices=BlocksVertices.getTypeVertices(blockType);
         for (int i=0;i<vertices.length;i++){
             vertices[i]=scale*vertices[i];
         }
         this.setVertices(vertices);
+    }
+    public void setTexture() {
+        switch (type) {
+            case Z:
+                blockImage = new Texture("44.png");
+            case NAIL:
+                blockImage = new Texture("22.png");
+            case L:
+                blockImage = new Texture("66.png");
+            case Z_INVERSE:
+                blockImage = new Texture("33.png");
+            case SQUARE:
+                blockImage = new Texture("11.png");
+            case L_INVERSE:
+                blockImage = new Texture("77.png");
+            case LINE:
+                blockImage = new Texture("55.png");
+        }
     }
     public void updatePosition(float dt) {
 
