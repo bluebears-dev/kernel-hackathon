911af627129288d7d66e4471cd0d14f06f507602
Patrycja Bruździńska <patrycja.bruzdzinska@gmail.com>
2017-11-25 10:31:03 +0100

    Adding block vertices and scaling.

--- core/src/com/hackathon/tetris/blocks/BlocksVertices.java
+++ core/src/com/hackathon/tetris/blocks/BlocksVertices.java
@@ -1,7 +1,7 @@
 package com.hackathon.tetris.blocks;
 
 public class BlocksVertices {
-    public float[] zVertices={1,0,
+    public static float[] zVertices={1,0,
                               3,0,
                               3,1,
                               2,1,
@@ -10,7 +10,7 @@ public class BlocksVertices {
                               0,1,
                               1,1,
                               1,0};
-    public float[] nailVertices={1,0,
+    public static float[] nailVertices={1,0,
                                  2,0,
                                  2,1,
                                  3,1,
@@ -19,14 +19,14 @@ public class BlocksVertices {
                                  0,1,
                                  1,1,
                                  1,0};
-    public float[] lVertices={0,0,
+    public static float[] lVertices={0,0,
                               3,0,
                               3,2,
                               2,2,
                               2,1,
                               0,1,
                               0,0};
-    public float[] zInverseVertices={0,0,
+    public static float[] zInverseVertices={0,0,
                                      2,0,
                                      2,1,
                                      3,1,
@@ -35,21 +35,33 @@ public class BlocksVertices {
                                      1,1,
                                      0,1,
                                      0,0};
-    public float[] squareVertices={0,0,
+    public static float[] squareVertices={0,0,
                                    2,0,
                                    2,2,
                                    0,2,
                                    0,0};
-    public float[] lInverseVertices={2,0,
+    public static float[] lInverseVertices={2,0,
                                      3,0,
                                      3,2,
                                      0,2,
                                      0,1,
                                      2,1,
                                      2,0};
-    public float[] lineInverse={0,0,
-                                4,0,
-                                4,1,
-                                0,4,
-                                0,0};
+    public static float[] lineVertices={0,0,
+                                        4,0,
+                                        4,1,
+                                        0,4,
+                                        0,0};
+    public static float[] getTypeVertices(BlocksTypesEnum type) {
+        switch (type) {
+            case Z: return zVertices;
+            case NAIL: return nailVertices;
+            case L: return lVertices;
+            case Z_INVERSE: return zInverseVertices;
+            case SQUARE: return squareVertices;
+            case L_INVERSE: return lInverseVertices;
+            case LINE: return lineVertices;
+        }
+        return null;
+    }
 }
