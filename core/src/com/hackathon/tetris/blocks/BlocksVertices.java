dbffd06110e917df14df2e5b4a6e17069900a029
Patrycja Bruździńska <patrycja.bruzdzinska@gmail.com>
2017-11-25 10:16:51 +0100

    Adding block vertices.

---
+++ core/src/com/hackathon/tetris/blocks/BlocksVertices.java
@@ -0,0 +1,55 @@
+package com.hackathon.tetris.blocks;
+
+public class BlocksVertices {
+    public float[] zVertices={1,0,
+                              3,0,
+                              3,1,
+                              2,1,
+                              2,2,
+                              0,2,
+                              0,1,
+                              1,1,
+                              1,0};
+    public float[] nailVertices={1,0,
+                                 2,0,
+                                 2,1,
+                                 3,1,
+                                 3,2,
+                                 0,2,
+                                 0,1,
+                                 1,1,
+                                 1,0};
+    public float[] lVertices={0,0,
+                              3,0,
+                              3,2,
+                              2,2,
+                              2,1,
+                              0,1,
+                              0,0};
+    public float[] zInverseVertices={0,0,
+                                     2,0,
+                                     2,1,
+                                     3,1,
+                                     3,2,
+                                     1,2,
+                                     1,1,
+                                     0,1,
+                                     0,0};
+    public float[] squareVertices={0,0,
+                                   2,0,
+                                   2,2,
+                                   0,2,
+                                   0,0};
+    public float[] lInverseVertices={2,0,
+                                     3,0,
+                                     3,2,
+                                     0,2,
+                                     0,1,
+                                     2,1,
+                                     2,0};
+    public float[] lineInverse={0,0,
+                                4,0,
+                                4,1,
+                                0,4,
+                                0,0};
+}
