213c2fffae6e2f78ccab5f3d50ca52f38deb7c12
Paweł Górski <pgorski42@gmail.com>
2017-11-25 08:19:08 +0100

    Init

---
+++ desktop/src/com/hackathon/tetris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hackathon.tetris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hackathon.tetris.TetrisGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TetrisGame(), config);
+	}
+}
